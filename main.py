6da7eaae2a33e74207304e505d3ab08afb8448ce
depth172 <depth172@yahoo.co.jp>
2023-12-26 06:05:57 +0900

    added nightfall information post

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from src import *
 from zoneinfo import ZoneInfo
 import datetime
 import src.tweet as tw
+import re
 
 gotInfo = False
 checkCode = 0
@@ -11,60 +12,83 @@ TimeZone = ZoneInfo("Asia/Tokyo")
 
 print("定期実行を開始します。\n")
 
-currentStatus = redis.from_url(url=os.getenv('REDIS_URL'))
+data = redis.from_url(url=os.getenv('REDIS_URL'))
 
 currentTime = datetime.datetime.now(TimeZone)
 
-# 毎日2時にセクター取得フラグをリセット
-if currentTime.hour == 2:
-    currentStatus.set('gotSector', 'False')
-    # 水曜日だった場合は週間フラグもリセット
-    if currentTime.weekday() == 2:
-        currentStatus.set('gotXur', 'False')
-# 毎日9時のバンシー取得フラグをリセットする
-elif currentTime.hour == 9:
-    currentStatus.set('gotBanshee', 'False')
+# 直前実行時の時間を取得
+lastHour = re.findall(r'\d+', str(data.get('lastHour')))[0]
+
+# 実行時の時間を取得
+currentHour = currentTime.hour
+
+if lastHour != currentHour:
+    data.set('lastHour', currentHour)
+    # 毎日2時にデイリー取得フラグをリセット
+    if currentHour == 2:
+        data.set('lastHour', currentHour)
+        # 水曜日だった場合は週間フラグもリセット
+        if currentTime.weekday() == 2:
+            data.set('gotXur', 'False')
+            data.set('gotNightfall', 'False')
+    # 毎日9時にバンシー取得フラグをリセットする
+    elif currentHour == 9:
+        data.set('gotBanshee', 'False')
+else:
+    print("情報の更新はまだ行いません。")
 
 # シーズンはじめの一時処理
-# currentStatus.set('gotSector', 'True')
+# data.set('gotSector', 'True')
 
 # 土曜～火曜かつ、シュールに関する情報を未取得だった場合は取得
-if currentTime.weekday() in [0, 1, 5, 6] and currentStatus.get('gotXur') == b'False':
+if currentTime.weekday() in [0, 1, 5, 6] and data.get('gotXur') == b'False':
     gotInfo = True
     print("シュールの情報を取得します。")
     checkCode = xur.getXur()
     if checkCode == 0:
-        currentStatus.set('gotXur', 'True')
+        data.set('gotXur', 'True')
     elif checkCode == 5:
         timeStr = currentTime.strftime('%Y/%m/%d')
         tweetText = "【ベンダー情報】" + timeStr + "\nAPIのメンテナンス中につき、シュールの販売アイテムに関する情報が取得できませんでした。後ほど再試行致しますので、しばらくお待ち下さい。"
         contents = {"text": tweetText}
         tw.makeTweet(contents)
-
-# バンシー44の販売武器に関する情報を未取得だった場合は取得
-elif currentStatus.get('gotBanshee') == b'False':
+        
+# ナイトフォールの情報を未取得だった場合は取得
+if data.get('gotNightfall') == b'False':
     gotInfo = True
-    print("バンシー44の「注目」武器情報を取得します。")
-    checkCode = banshee.getBanshee()
+    print("ナイトフォールの情報を取得します。")
+    checkCode = nightfall.getNightfall()
     if checkCode == 0:
-        currentStatus.set('gotBanshee', 'True')
+        data.set('gotNightfall', 'True')
     elif checkCode == 5:
         timeStr = currentTime.strftime('%Y/%m/%d')
-        tweetText = "【ベンダー情報】" + timeStr + "\nAPIのメンテナンス中につき、バンシー44の日間販売武器に関する情報が取得できませんでした。後ほど再試行致しますので、しばらくお待ち下さい。"
+        tweetText = "【ナイトフォール情報】" + timeStr + "\nAPIのメンテナンス中につき、ナイトフォールに関する情報が取得できませんでした。後ほど再試行致しますので、しばらくお待ち下さい。"
         contents = {"text": tweetText}
         tw.makeTweet(contents)
 
-
 # 失われたセクターの情報を未取得だった場合は取得
-if currentStatus.get('gotSector') == b'False':
+if data.get('gotSector') == b'False':
     gotInfo = True
     print("失われたセクターの情報を取得します。")
     checkCode = lostsector.getLostSector()
     if checkCode == 0:
-        currentStatus.set('gotSector', 'True')
+        data.set('gotSector', 'True')
     elif checkCode == 5:
         timeStr = currentTime.strftime('%Y/%m/%d')
         tweetText = "【失われたセクター情報】" + timeStr + "\nAPIのメンテナンス中につき、失われたセクター(伝説/達人)に関する情報が取得できませんでした。後ほど再試行致しますので、しばらくお待ち下さい。"
+        contents = {"text": tweetText}
+        tw.makeTweet(contents)
+        
+# バンシー44の販売武器に関する情報を未取得だった場合は取得
+if data.get('gotBanshee') == b'False':
+    gotInfo = True
+    print("バンシー44の販売武器情報を取得します。")
+    checkCode = banshee.getBanshee()
+    if checkCode == 0:
+        data.set('gotBanshee', 'True')
+    elif checkCode == 5:
+        timeStr = currentTime.strftime('%Y/%m/%d')
+        tweetText = "【ベンダー情報】" + timeStr + "\nAPIのメンテナンス中につき、バンシー44の日間販売武器に関する情報が取得できませんでした。後ほど再試行致しますので、しばらくお待ち下さい。"
         contents = {"text": tweetText}
         tw.makeTweet(contents)
         
---
+++ src/nightfall.py
@@ -0,0 +1,237 @@
+### ライブラリのインポート ###
+# 認証関連
+import src.auth as auth
+import os
+# API取得用
+import requests
+# 画像処理用
+from PIL import Image, ImageDraw, ImageFont
+import io
+# ツイート用
+from . import tweet as tw
+# 細かい処理用
+from time import sleep
+import datetime
+from zoneinfo import ZoneInfo
+import csv
+import math
+
+def getNightfall():
+    # 頻出する辞書とリストの定義
+    champion = {'barrier': "バリア", 'unstoppable': "アンストッパブル", 'overload': "オーバーロード"}
+    elemHash = {'solar': "1847026933", 'arc': "2303181850", 'void': "3454344768", 'stasis': "151347233", 'strand': "3949783978"}
+
+    # 画像生成用のフォント定義
+    fontS = ImageFont.truetype('./.font/GlowSansSC-Normal-Medium.otf', 17)
+    fontN = ImageFont.truetype('./.font/GlowSansSC-Normal-Medium.otf', 25)
+    fontB0 = ImageFont.truetype('./.font/GlowSansSC-Normal-Bold.otf', 28)
+    fontB1 = ImageFont.truetype('./.font/GlowSansSC-Normal-Bold.otf', 32)
+    fontB2 = ImageFont.truetype('./.font/GlowSansSC-Normal-Bold.otf', 40)
+    fontTitle = ImageFont.truetype('./.font/GlowSansSC-Normal-Bold.otf', 59)
+    logoImg = Image.open("./img/logo_full.png").resize((300, 46), 1)
+
+    ### アクセスに必要なトークンを取得 ###
+    auth.getBungieAccessToken()
+
+    #### 基本データの取得 ####
+    # ヘッダーにAPIキーとアクセストークンを設定
+    headers = {"X-API-Key": os.getenv('B_API_KEY'),
+            "Authorization": "Bearer " + os.environ["BAPI_ACCESS_TOKEN"]}
+
+    # 日付データの準備
+    TimeZone = ZoneInfo("Asia/Tokyo")
+    todayDate = datetime.datetime.now(TimeZone).date()
+    todayDateStr = todayDate.strftime('%Y/%m/%d')
+    endDate = todayDate + datetime.timedelta(days=8-todayDate.weekday())
+    endDateStr = endDate.strftime('%Y/%m/%d')
+    
+    # ナイトフォールの情報を取得
+    # 取得成功可否も確認する
+    for i in range(3):
+        activityData = requests.get("https://www.bungie.net//Platform/Destiny2/Milestones/?lc=ja", headers=headers).json()
+        if 'Response' not in activityData:
+            for j in range(3):
+                if activityData['ErrorCode'] == 5:
+                    print("現在APIサービスはメンテナンス中です。処理を中断します。")
+            if i != 2:
+                print("API取得に失敗しました。3秒後にリトライします…")
+                sleep(3)
+                continue
+            else:
+                print("APIが取得できませんでした。処理を中断します。")
+                print("エラーコード - " + str(activityData['ErrorCode']) + "\n" + activityData['Message'])
+            return activityData['ErrorCode']
+        else:
+            break
+        
+    nightfallHash = activityData['Response']['2029743966']['activities'][3]['activityHash']
+    nightfallData = requests.get("https://www.bungie.net/Platform/Destiny2/Manifest/DestinyActivityDefinition/" + str(nightfallHash) + "/?lc=ja", headers=headers).json()
+    
+    # 各種情報の取得    
+    nightfallName = nightfallData['Response']['displayProperties']['description']
+
+    shieldHash = activityData['Response']['2029743966']['activities'][3]['modifierHashes'][0]
+    shieldData = requests.get("https://www.bungie.net/Platform/Destiny2/Manifest/DestinyActivityModifierDefinition/" + str(shieldHash) + "/?lc=ja", headers=headers).json()
+    shieldPath = shieldData['Response']['displayProperties']['icon']
+
+    championHash = activityData['Response']['2029743966']['activities'][3]['modifierHashes'][4]
+    championData = requests.get("https://www.bungie.net/Platform/Destiny2/Manifest/DestinyActivityModifierDefinition/" + str(championHash) + "/?lc=ja", headers=headers).json()
+    championPath = championData['Response']['displayProperties']['icon']
+
+    specialModHash = activityData['Response']['2029743966']['activities'][3]['modifierHashes'][10]
+    specialModData = requests.get("https://www.bungie.net/Platform/Destiny2/Manifest/DestinyActivityModifierDefinition/" + str(specialModHash) + "/?lc=ja", headers=headers).json()
+    specialModPath = specialModData['Response']['displayProperties']['icon']
+    specialModName = specialModData['Response']['displayProperties']['name']
+    specialModDesc = specialModData['Response']['displayProperties']['description']
+
+    threatHash = activityData['Response']['2029743966']['activities'][3]['modifierHashes'][6]
+    threatData = requests.get("https://www.bungie.net/Platform/Destiny2/Manifest/DestinyActivityModifierDefinition/" + str(threatHash) + "/?lc=ja", headers=headers).json()
+    threatPath = threatData['Response']['displayProperties']['icon']
+
+    surge1Hash = activityData['Response']['2029743966']['activities'][3]['modifierHashes'][13]
+    surge1Data = requests.get("https://www.bungie.net/Platform/Destiny2/Manifest/DestinyActivityModifierDefinition/" + str(surge1Hash) + "/?lc=ja", headers=headers).json()
+    surge1Path = surge1Data['Response']['displayProperties']['icon']
+
+    surge2Hash = activityData['Response']['2029743966']['activities'][3]['modifierHashes'][14]
+    surge2Data = requests.get("https://www.bungie.net/Platform/Destiny2/Manifest/DestinyActivityModifierDefinition/" + str(surge2Hash) + "/?lc=ja", headers=headers).json()
+    surge2Path = surge2Data['Response']['displayProperties']['icon']
+
+    ocHash = activityData['Response']['2029743966']['activities'][3]['modifierHashes'][15]
+    ocData = requests.get("https://www.bungie.net/Platform/Destiny2/Manifest/DestinyActivityModifierDefinition/" + str(ocHash) + "/?lc=ja", headers=headers).json()
+    ocPath = ocData['Response']['displayProperties']['icon']
+
+    # 報酬武器の取得
+    vendorData = requests.get("https://www.bungie.net/Platform/Destiny2/3/Profile/4611686018482984497/Character/2305843009405135001/Vendors/2232145065/?components=401,402", headers=headers).json()
+    weaponOrder = vendorData['Response']['categories']['data']['categories'][1]['itemIndexes'][-1]
+    weaponAdeptHash = vendorData['Response']['sales']['data'][str(weaponOrder)]['itemHash']
+    
+    # 検索にかけるため、英語名を作る（(Adept)つきで取得→(Adept)を切り落とす）
+    weaponDataEng = requests.get("https://www.bungie.net/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/" + str(weaponAdeptHash), headers=headers).json()
+    weaponNameRaw = weaponDataEng['Response']['displayProperties']['name']
+    weaponNameEng = weaponNameRaw[:-8]
+    
+    searchRes = requests.get("https://www.bungie.net/Platform/Destiny2/Armory/Search/DestinyInventoryItemDefinition/" + weaponNameEng + "/", headers=headers).json()
+    
+    # 検索で得た武器が今シーズンのもの、かつ新版でないか確認
+    # シーズン確認のためにwatermarkを比較するのはさすがにアレなのでもっといい案を考えたい
+    i = 0
+    while (1):
+        weaponHash = searchRes['Response']['results']['results'][i]['hash']
+        weaponData = requests.get("https://www.bungie.net/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/" + str(weaponHash), headers=headers).json()
+        if weaponData['Response']['iconWatermark'] == "/common/destiny2_content/icons/a2fb48090c8bc0e5785975fab9596ab5.png" and weaponData['Response']['displayProperties']['name'] not in "(Adept)":
+            break
+        else:
+            i += 1 
+    
+    weaponData = requests.get("https://www.bungie.net/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/" + str(weaponHash) + "/?lc=ja", headers=headers).json()
+
+    weaponName = weaponData['Response']['displayProperties']['name']
+    weaponSpec = weaponData['Response']['itemTypeDisplayName']
+    
+    tweetText = ""
+    mediaList = []
+
+    # ツイート用の文章を整形
+    tweetText = "【 #ナイトフォール 情報】" + todayDateStr + "\n今週のナイトフォールは「" + nightfallName + "」です。\n\n報酬: " + weaponName + "（" + weaponSpec + "）\n戦闘条件など、詳細は画像をご確認ください。\n\n#Destiny2"
+    
+    print(tweetText + "\n")
+
+    # 画像生成
+    imageURL = nightfallData['Response']['pgcrImage']
+    image = Image.open(io.BytesIO(requests.get("https://www.bungie.net" + imageURL).content)).convert('RGBA')
+    mask = Image.open("./img/mask.png")
+
+    baseImg = Image.alpha_composite(image, mask)
+    
+    resImg = io.BytesIO()
+    draw = ImageDraw.Draw(baseImg)
+
+    draw.text((40, 34), "ナイトフォール", fill=(255, 255, 255), font=fontB2)
+    draw.text((350, 45), "(" + todayDateStr + " ～ " + endDateStr + ")", fill=(255, 255, 255), font=fontB0)
+    draw.text((40, 113), nightfallName, fill=(255, 255, 255), font=fontTitle)
+    baseImg.paste(logoImg, (935, 45), logoImg)
+    
+    specialModImg = Image.open(io.BytesIO(requests.get("https://www.bungie.net" + specialModPath).content)).convert("RGBA").resize((60, 60), 1)
+    baseImg.paste(specialModImg, (35, 223), specialModImg)
+    draw.text((112, 228), specialModName, fill=(255, 255, 255), font=fontB1)
+    draw.text((50, 296), specialModDesc, fill=(255, 255, 255), font=fontN)
+
+    draw.multiline_text((35, 360), "＜戦闘条件＞", fill=(255, 255, 255), font=fontB1)
+
+    draw.text((35, 410), "シールド", fill=(255, 255, 255), font=fontB0)
+    shieldImg = Image.open(io.BytesIO(requests.get("https://www.bungie.net" + shieldPath).content)).convert("RGBA").resize((80, 80), 1)
+    baseImg.paste(shieldImg, (46, 460), shieldImg)
+
+    draw.text((194, 411), "脅威", fill=(255, 255, 255), font=fontB0)
+    threatImg = Image.open(io.BytesIO(requests.get("https://www.bungie.net" + threatPath).content)).convert("RGBA").resize((80, 80), 1)
+    baseImg.paste(threatImg, (180, 460), threatImg)
+
+    draw.text((70, 550), "チャンピオン", fill=(255, 255, 255), font=fontB0)
+    championImg = Image.open(io.BytesIO(requests.get("https://www.bungie.net" + championPath).content)).convert("RGBA").resize((80, 80), 1)
+    baseImg.paste(championImg, (113, 600), championImg)
+
+    draw.text((384, 410), "サージ", fill=(255, 255, 255), font=fontB0)
+    surge1Img = Image.open(io.BytesIO(requests.get("https://www.bungie.net" + surge1Path).content)).convert("RGBA").resize((80, 80), 1)
+    surge2Img = Image.open(io.BytesIO(requests.get("https://www.bungie.net" + surge2Path).content)).convert("RGBA").resize((80, 80), 1)
+    baseImg.paste(surge1Img, (339, 460), surge1Img)
+    baseImg.paste(surge2Img, (429, 460), surge2Img)
+
+    draw.text((314, 550), "オーバーチャージ", fill=(255, 255, 255), font=fontB0)
+    ocImg = Image.open(io.BytesIO(requests.get("https://www.bungie.net" + ocPath).content)).convert("RGBA").resize((80, 80), 1)
+    baseImg.paste(ocImg, (384, 600), ocImg)
+
+    draw.multiline_text((605, 360), "＜限定報酬＞", fill=(255, 255, 255), font=fontB1)
+
+    ## 武器アイコン挿入
+    # パスから画像・ウォーターマークを取得
+    weaponImgPath = weaponData['Response']['displayProperties']['icon']
+    weaponWMPath = weaponData['Response']['quality']['displayVersionWatermarkIcons'][-1]
+    weaponBaseImg = Image.open(io.BytesIO(requests.get("https://www.bungie.net" + weaponImgPath).content))
+    weaponBaseImg.putalpha(255)
+    weaponWM = Image.open(io.BytesIO(requests.get("https://www.bungie.net" + weaponWMPath).content)).convert("RGBA")
+    # 武器画像とウォーターマークを合成
+    weaponImg = Image.alpha_composite(weaponBaseImg, weaponWM)
+    # リサイズして挿入
+    weaponImg = weaponImg.resize((100, 100), 1)
+    baseImg.paste(weaponImg, (650, 475))
+
+    ## 武器名挿入
+    draw.multiline_text((770, 480), weaponName, fill=(255, 255, 255), font=fontB0)
+
+    ## 属性アイコン挿入
+    weaponElemHash = weaponData['Response']['damageTypeHashes'][0]
+    if weaponElemHash != 3373582085:
+        # 属性アイコンがある場合は間隔を空ける
+        elementShift = 42
+        weaponElemData = requests.get("https://www.bungie.net/Platform/Destiny2/Manifest/DestinyDamageTypeDefinition/" + str(weaponElemHash) + "/?lc=ja", headers=headers).json()
+        weaponElemPath = weaponElemData['Response']['displayProperties']['icon']
+        weaponElemImg = Image.open(io.BytesIO(requests.get("https://www.bungie.net" + weaponElemPath).content)).convert("RGBA").resize((30, 30), 1)
+        baseImg.paste(weaponElemImg, (770, 527), weaponElemImg)
+    else:
+        elementShift = 0
+    
+    ## 弾薬アイコン挿入
+    if weaponData['Response']['equippingBlock']['ammoType'] == 1:
+        weaponAmmoImg = Image.open(io.BytesIO(requests.get("https://www.bungie.net/common/destiny2_content/icons/99f3733354862047493d8550e46a45ec.png").content)).convert("RGBA").resize((50, 50), 1)
+    elif weaponData['Response']['equippingBlock']['ammoType'] == 2:
+        weaponAmmoImg = Image.open(io.BytesIO(requests.get("https://www.bungie.net/common/destiny2_content/icons/d920203c4fd4571ae7f39eb5249eaecb.png").content)).convert("RGBA").resize((50, 50), 1)
+    elif weaponData['Response']['equippingBlock']['ammoType'] == 3:
+        weaponAmmoImg = Image.open(io.BytesIO(requests.get("https://www.bungie.net/common/destiny2_content/icons/78ef0e2b281de7b60c48920223e0f9b1.png").content)).convert("RGBA").resize((50, 50), 1)
+    baseImg.paste(weaponAmmoImg, (764 + elementShift, 517), weaponAmmoImg)
+
+    ## 武器種挿入
+    weaponArchName = weaponData['Response']['itemTypeDisplayName']
+    draw.multiline_text((819 + elementShift, 522), weaponArchName, fill=(255, 255, 255), font=fontN)
+    
+    draw.text((1250, 680), "＊ 玄人クリアで、上記武器の“新 ・ ”版がドロップします。", fill=(255, 255, 255), font=fontN, anchor='rb')
+
+    baseImg.show()
+    baseImg.convert("RGB").save(resImg, format='JPEG')
+    mediaList.append(tw.postImage(resImg.getvalue()))
+    
+    content = {"text": tweetText, "media": {"media_ids": mediaList}}
+    tw.makeTweet(content)
+
+    print("情報取得の全工程完了。")
+    
+    return 0
